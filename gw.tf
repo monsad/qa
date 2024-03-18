dd936dc48662d80b7a20decd436ad7c6d71940ca
Monika Sadlok <monikaksadlok@gmail.com>
2024-03-18 14:36:53 +0100

    add terraform

---
+++ gw.tf
@@ -0,0 +1,4 @@
+resource "aws_internet_gateway" "test-env-gw" {
+  vpc_id = "${aws_vpc.test-env.id}"
+
+}
---
+++ variables.tf
@@ -0,0 +1,6 @@
+variable "ami_name" {
+  type = string
+  default = "ami-0b9932f4918a00c4f"
+}
+variable "ami_id" {}
+variable "ami_key_pair_name" {}
